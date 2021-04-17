233e9940e962afff257f6b25f3efd44df5bef1a7
palagpatski <palagpatski@gmail.com>
2021-04-17 20:08:56 +0800

    User form styling

--- js/wwm_adminimal.scripts.js
+++ js/wwm_adminimal.scripts.js
@@ -2,17 +2,17 @@
   /**
    * Toolbar Spacing
    */
-  Drupal.behaviors.labelWidths = {
-    attach: function(context, settings) {
-      function labelWidths() {
-        $('.form-type-textfield, .form-type-number, .form-type-select, .form-type-email, .form-type-password, .form-type-tel').each(function(index) {
-          var labelWidth = $(this).find('label').width();
-          $(this).find('input').style.setProperty('--label-width', 'labelWidth');
-        });
-      }
+  // Drupal.behaviors.labelWidths = {
+  //   attach: function(context, settings) {
+  //     function labelWidths() {
+  //       $('.form-type-textfield, .form-type-number, .form-type-select, .form-type-email, .form-type-password, .form-type-tel').each(function(index) {
+  //         var labelWidth = $(this).find('label').width();
+  //         $(this).find('input').style.setProperty('--label-width', 'labelWidth');
+  //       });
+  //     }
 
-      $(window).resize(labelWidths);
-    }
-  }; 
+  //     $(window).resize(labelWidths);
+  //   }
+  // }; 
 
 })(jQuery, Drupal);
