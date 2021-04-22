af6153912f371b6c7e801bcc085ae3fb1aed37a6
palagpatski <palagpatski@gmail.com>
2021-04-22 11:45:18 +0800

    Improve script

--- js/adminimal_wwm.scripts.js
+++ js/adminimal_wwm.scripts.js
@@ -7,7 +7,7 @@
       function labelWidths() {
         $('.form-type-textfield, .form-type-number, .form-type-select, .form-type-email, .form-type-password').each(function() {
           var labelWidth = $(this).find('label').outerWidth(true) + 'px';
-          $(this).find('input').css('flex', 'calc(100% - ' + labelWidth + ')');
+          $(this).find('input').css('--labelWidth', labelWidth);
         });
       }
 
