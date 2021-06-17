03a9037fa4bc4b52796e1555c74f0c079dedb7cb
palagpatski <palagpatski@gmail.com>
2021-06-17 12:43:18 +0800

    Improve automatic field width calculation

--- js/adminimal_wwm.scripts.js
+++ js/adminimal_wwm.scripts.js
@@ -2,22 +2,23 @@
   /**
    * Automatic Field width resizing
    */
-  // Drupal.behaviors.labelWidths = {
-  //   attach: function(context, settings) {
-  //     function labelWidths() {
-  //       $('.form-type-textfield, .form-type-number, .form-type-select, .form-type-email, .form-type-password').each(function() {
-  //         var labelWidth = $(this).find('label').outerWidth(true) + 'px';
-  //         $(this).find('input').css('--labelWidth', labelWidth);
-  //       });
-  //     }
-  //
-  //     $(window).on("load resize mresize", function(){
-  //       labelWidths;
-  //     });
-  //     setInterval(labelWidths, 1000);
-  //     clearInterval(10000);
-  //   }
-  // };
+  Drupal.behaviors.labelWidths = {
+    attach: function(context, settings) {
+      function labelWidths() {
+        $('.form-type-textfield, .form-type-number, .form-type-select, .form-type-email, .form-type-password').each(function() {
+          var labelWidth = $(this).find('label').outerWidth(true) + 'px';
+          $(this).find('input').css('--labelWidth', labelWidth);
+        });
+      }
+
+      labelWidths();
+      $(window).on("load resize mresize", function(){
+        labelWidths;
+      });
+      setInterval(labelWidths, 1000);
+      clearInterval(10000);
+    }
+  };
 
   /**
    * Sticky panel
